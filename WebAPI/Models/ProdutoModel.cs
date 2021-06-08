96f3867a21e4dbd87d5988168fdf882bb7991eee
João Vitor <jv-santos98@hotmail.com>
2021-06-08 19:32:33 -0300

    Cria model

---
+++ WebAPI/Models/ProdutoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebAPI.Models
+{
+    public class ProdutoModel
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
